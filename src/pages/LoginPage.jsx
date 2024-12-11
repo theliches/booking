b389e40807e4caa14bb09be0d4ac3f491d571e4c
JessicaNatasja <jessicanatasja02@gmail.com>
2024-12-11 23:06:11 +0100

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,20 +4,16 @@ import Header from "../components/common/Header";
 
 
 
-const OrdersPage = () => {
+const LoginPage = () => {
 	return (
 		<div className='flex-1 relative z-10 overflow-auto'>
-			<Header title={"Orders"} />
+			<Header title={"Login"} />
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-				
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
-					
 				</div>
-
-				
 			</main>
 		</div>
 	);
 };
-export default OrdersPage;
+export default LoginPage;
